d697c878a43e08317b3deb5cec5c07a12ebccc55
almoukdad <almoukdad88@gmail.com>
2018-04-04 23:26:54 +0300

    notes

---
+++ forEach.js
@@ -0,0 +1,25 @@
+//    arr.forEach(someFunction)
+
+var colors=['red','green','blue','yellow'];
+
+colors.forEach(function(){
+	console.log('INSIDE THE FOREACH');
+});
+
+
+colors.forEach(function(iLoveDogs){
+	console.log('INSIDE THE FOREACH' + iLoveDogs);
+});
+
+function printColor(color){
+	console.log("*********");
+	console.log(color);
+	console.log("*********");
+};
+// printColor("purple")  => 
+
+
+//now 
+colors.forEach(printColor);
+printColor(colors[1]);
+printColor("green");
