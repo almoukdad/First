611597e9a5355a33b8fd00bd0a7e79889882d811
almoukdad <almoukdad88@gmail.com>
2018-04-11 18:55:19 +0300

    Small Movies DB

---
+++ moviesDB.js
@@ -0,0 +1,50 @@
+//==== Movies Database ==
+
+var movies=[						// it's a small sample of DB
+	{
+		title:"In Bruges",
+		hasWatched:true,
+		rating:5
+	},
+	{
+		title:"Frozen",
+		hasWatched:false,
+		rating:4.5
+	},
+	{
+		title:"Titanic",
+		hasWatched: true,
+		rating: 4.8
+	}
+]
+
+movies.forEach(function(movie){
+var results = "You Have ";
+	if(movie.hasWatched){
+		results+= "Watched";
+	} else{
+		results+= "Not Seen";
+	}
+	results += " '" + movie.title + "' - " + movie.rating + " Stars";
+	alert(results);
+	
+})
+// I can pass the conditional statements inside a function and then invoke that 
+// function fron inside the forEach loop !
+// Let's see ..
+function buildString(movie){
+	var results = "You Have ";
+	if(movie.hasWatched){
+		results+= "Watched";
+	} else{
+		results+= "Not Seen";
+	}
+	results += " '" + movie.title + "' - " + movie.rating + " Stars";
+	return results;
+}
+
+// let's write the same forEach with invoking the function 
+
+movies.forEach(function(movie){
+	console.log(buildString(movie));
+});
